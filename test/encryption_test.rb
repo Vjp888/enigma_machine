ca3b1c658e082878f6fa691ccc9a10fd3f273179
vjp888 <rellik888@gmail.com>
2018-11-03 14:39:53 -0600

    creates test helper file and initial test for encryption

---
+++ test/encryption_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class EncryptionTest < MiniTest::Test
+
+  def test_it_exists
+    encrypt = Encryption.new("Encrypt Me", 12345, Time.now)
+
+    assert_instance_of Encryption, encrypt
+  end
+end
