16a90ae92536d0528d66efee78dce48e61b08223
vjp888 <rellik888@gmail.com>
2018-11-03 16:22:57 -0600

    Adds create rotation groups test for encryption class

--- test/encryption_test.rb
+++ test/encryption_test.rb
@@ -7,4 +7,11 @@ class EncryptionTest < MiniTest::Test
 
     assert_instance_of Encryption, encrypt
   end
+
+  def test_it_seperates_key_into_rotation_groups
+    encrypt = Encryption.new("Encrypt Me", 12345, Time.now)
+    offset = [12,23,34,45]
+
+    assert_equal offset, encrypt.create_offset
+  end
 end
