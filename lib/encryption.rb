193ad3b6bf02d6c284d3aa00c27626531e774fe3
vjp888 <rellik888@gmail.com>
2018-11-03 20:39:29 -0600

    Shortens the encrypt method by over half

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -46,20 +46,27 @@ class Encryption
     encrypt_key = create_encryption_key
     key_pos = 0
     @message.downcase.split(//).map do |letter|
-      if key_pos <=3
-        char_num = char_map.find_index("#{letter}")
-        encrypt_letter = char_map.rotate(encrypt_key[key_pos])[char_num]
-        encrypted_message << encrypt_letter
-        key_pos += 1
-      else
+      char_num = char_map.find_index("#{letter}")
+      encrypt_letter = char_map.rotate(encrypt_key[key_pos])[char_num]
+      encrypted_message << encrypt_letter
+      key_pos += 1
+      if key_pos == 4
         key_pos = 0
-        char_num = char_map.find_index("#{letter}")
-        encrypt_letter = char_map.rotate(encrypt_key[key_pos])[char_num]
-        encrypted_message << encrypt_letter
-        key_pos += 1
       end
     end
     {encryption: encrypted_message.join, key: @key, date: @date}
   end
-
 end
+    #   if key_pos <=3
+    #     char_num = char_map.find_index("#{letter}")
+    #     encrypt_letter = char_map.rotate(encrypt_key[key_pos])[char_num]
+    #     encrypted_message << encrypt_letter
+    #     key_pos += 1
+    #   else
+    #     key_pos = 0
+    #     char_num = char_map.find_index("#{letter}")
+    #     encrypt_letter = char_map.rotate(encrypt_key[key_pos])[char_num]
+    #     encrypted_message << encrypt_letter
+    #     key_pos += 1
+    #   end
+    # end
