7bb6f1fdfa186ae1eccd2ebe770e26f6c8b99a83
vjp888 <rellik888@gmail.com>
2018-11-05 09:08:04 -0700

    Adds working rotation groups method

--- lib/decryption.rb
+++ lib/decryption.rb
@@ -6,4 +6,10 @@ class Decryption
     @key = key
     @date = date
   end
+
+  def create_rotation_gourps
+    split = @key.split(//)
+    [(split[0] + split[1]).to_i, (split[1] + split[2]).to_i,
+    (split[2] + split[3]).to_i, (split[3] + split[4]).to_i]
+  end
 end
