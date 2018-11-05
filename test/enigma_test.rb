d947ecd46aa73dc965620ed48b8370eb99363d15
vjp888 <rellik888@gmail.com>
2018-11-05 10:22:50 -0700

    Adds test for enimga to encrypt a message

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -7,4 +7,13 @@ class EnigmaTest < MiniTest::Test
 
     assert_instance_of Enigma, enigma
   end
+
+  def test_enigma_can_encrypt_message
+    enigma = Enigma.new
+    enigma.encrypt("hello", "12345", "251218")
+
+    assert_equal "wfugc", enigma.encrypt[:encryption]
+    assert_equal "12345", enigma.encrypt[:key]
+    assert_equal "251218", enigma.encrypt[:date]
+  end
 end
