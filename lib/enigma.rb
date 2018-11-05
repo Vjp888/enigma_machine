832531b6b0c4e86a5146ff27f16d221202e7d8f4
vjp888 <rellik888@gmail.com>
2018-11-05 11:13:37 -0700

    Adds working encrypt method to enigma.

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,3 +1,7 @@
 class Enigma
 
+  def encrypt(message, key, date)
+    encrypt = Encryption.new(message, key, date)
+    encrypt.encrypt
+  end
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -10,10 +10,10 @@ class EnigmaTest < MiniTest::Test
 
   def test_enigma_can_encrypt_message
     enigma = Enigma.new
-    enigma.encrypt("hello", "12345", "251218")
+    encrypt = enigma.encrypt("hello", "12345", "251218")
 
-    assert_equal "wfugc", enigma.encrypt[:encryption]
-    assert_equal "12345", enigma.encrypt[:key]
-    assert_equal "251218", enigma.encrypt[:date]
+    assert_equal "wfugc", encrypt[:encryption]
+    assert_equal "12345", encrypt[:key]
+    assert_equal "251218", encrypt[:date]
   end
 end
