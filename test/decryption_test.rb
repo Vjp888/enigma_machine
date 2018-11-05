209c69018f7dc1bc68c7300fa2d4313ce19f4ae2
vjp888 <rellik888@gmail.com>
2018-11-05 09:06:47 -0700

    Adds rotaion groups test for key splitting

--- test/decryption_test.rb
+++ test/decryption_test.rb
@@ -8,4 +8,12 @@ class DecryptionTest < MiniTest::Test
     assert_instance_of Decryption, decrypt
   end
 
+  def test_it_can_create_rotation_groups
+    decrypt = Decryption.new("Message", "12345", "251218")
+    offset = [12,23,34,45]
+
+    assert_equal offset, decrypt.create_rotation_gourps
+    assert_equal 4, decrypt.create_rotation_gourps.length
+  end
+
 end
