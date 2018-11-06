b0fc026a74d7b27cb35ed0995f65af59c4b19925
vjp888 <rellik888@gmail.com>
2018-11-05 17:06:02 -0700

    Removes encryption key and renames to crypto_key to all function with module. Adjusts tests to call correct method.

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -11,24 +11,12 @@ class Encryption
     @char_map = ("a".."z").to_a << " "
   end
 
-  def create_offsets
-    date_square = @date.to_i ** 2
-    offset = (date_square % 10000).to_s.split(//)
-    offset.collect{|num| num.to_i}
-  end
-
-  def encryption_key
-    create_rotation_gourps.zip(create_offsets).map do |key|
-      key.sum
-    end
-  end
-
   def char_num(letter)
     @char_map.find_index("#{letter}")
   end
 
   def key_rotation(position)
-    @char_map.rotate(encryption_key[position])
+    @char_map.rotate(crypto_key[position])
   end
 
   def encrypt
