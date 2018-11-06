a5a03afc2d2febd10e10dfd16beb1c88bb41d3f3
vjp888 <rellik888@gmail.com>
2018-11-05 17:04:07 -0700

    Removes check key and rotation groups from encryption. Passes tests

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -11,19 +11,6 @@ class Encryption
     @char_map = ("a".."z").to_a << " "
   end
 
-  def check_key(key)
-    until key.length == 5
-      key.insert(0, '0')
-    end
-    key
-  end
-
-  def create_rotation_gourps
-    split = @key.split(//)
-    [(split[0] + split[1]).to_i, (split[1] + split[2]).to_i,
-    (split[2] + split[3]).to_i, (split[3] + split[4]).to_i]
-  end
-
   def create_offsets
     date_square = @date.to_i ** 2
     offset = (date_square % 10000).to_s.split(//)
